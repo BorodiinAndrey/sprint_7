75c7e61f6b0424b31863216a33641ec87e8c0a3b
Andrei Borodin <andrei.borodin@dcsportsoft.com>
2025-06-06 18:29:25 +0300

    Add test_login_courier_invalid_data

--- tests/test_create_courier.py
+++ tests/test_create_courier.py
@@ -5,17 +5,17 @@ from data.endpoints import CREATE_COURIER_URL
 from helpers.courier_helper import register_new_courier_and_return_login_password
 
 
-@allure.feature('Проверка создания курьера')
+@allure.feature('Проверка регистрации курьера')
 class TestCreateCourier:
 
-    @allure.title('Тест на создание курьера')
+    @allure.title('Тест регистрации курьера')
     def test_create_courier_success(self):
         with allure.step('Регистрация курьера'):
             creds = register_new_courier_and_return_login_password()
         with allure.step('Проверка, что креды вернулись'):
             assert creds, "Регистрация не осуществлена, вернулся пустой список и код 400"
 
-    @allure.title('Тест на создание курьеров с одинаковыми данными')
+    @allure.title('Тест регистрации курьеров с одинаковыми данными')
     def test_create_same_couriers(self):
         with allure.step('Регистрация курьера'):
             creds = register_new_courier_and_return_login_password()
@@ -24,19 +24,19 @@ class TestCreateCourier:
         with allure.step('Создание payload'):
             payload = {'login': login, 'password': password}
 
-        with allure.step('Создание курьера с теми же кредами'):
-            create_courier = requests.post(CREATE_COURIER_URL, json=payload)
+        with allure.step('Регистрация курьера с теми же кредами'):
+            response = requests.post(CREATE_COURIER_URL, json=payload)
 
         with allure.step('Проверка, что вернулся код 409'):
-            assert create_courier.status_code == 409, f"Получили код: {create_courier.status_code}"
+            assert response.status_code == 409, f"Получили код: {response.status_code}"
 
         with allure.step('Проверка сообщения'):
             expected_message = "Этот логин уже используется. Попробуйте другой."
-            actual_message = create_courier.json().get("message")
+            actual_message = response.json().get("message")
 
             assert actual_message == expected_message, f"Получили message: {actual_message}"
 
-    @allure.title('Тест с невалидными данными')
+    @allure.title('Тест регистрации курьера с невалидными данными')
     @pytest.mark.parametrize(
         'login, password',
         [
@@ -48,14 +48,14 @@ class TestCreateCourier:
     def test_create_couriers_with_invalid_data(self, login, password):
         with allure.step('Формирование payload'):
             payload = {"login": login, "password": password}
-        with allure.step('Регистрация курьера'):
-            create_courier = requests.post(CREATE_COURIER_URL, json=payload)
+        with allure.step(f'Регистрация курьера c payload {payload}'):
+            response = requests.post(CREATE_COURIER_URL, json=payload)
 
         with allure.step('Проверка, что вернулся код 400'):
-            assert create_courier.status_code == 400, f"Получили код: {create_courier.status_code}"
+            assert response.status_code == 400, f"Получили код: {response.status_code}"
 
         with allure.step('Проверка сообщения'):
             expected_message = "Недостаточно данных для создания учетной записи"
-            actual_message = create_courier.json().get("message")
+            actual_message = response.json().get("message")
 
             assert actual_message == expected_message, f"Получили message: {actual_message}"
--- tests/test_login_courier.py
+++ tests/test_login_courier.py
@@ -7,6 +7,7 @@ from helpers.courier_helper import register_new_courier_and_return_login_passwor
 
 @allure.feature('Проверка авторизации курьера')
 class TestLoginCourier:
+
     @allure.title('Тест на авторизацию курьера')
     def test_login_courier_success(self):
         with allure.step('Регистрация курьера'):
@@ -20,5 +21,35 @@ class TestLoginCourier:
 
         with allure.step('Проверка, что пришел код 200'):
             assert 200 == response.status_code, f'Пришел код: {response.status_code}'
-        with allure.step('Проверка, что присвоен id'):
+
+        with allure.step('Проверка, что в ответе присутствует ID'):
             assert 'id' in response.json(), f'ID не присвоен'
+
+    @allure.title('Тест на авторизацию курьера с невалидными данными')
+    @pytest.mark.parametrize(
+        'login, password, expected_status, expected_message',
+        [
+            (None, None, 400, 'Недостаточно данных для входа'),
+            ('', '', 400, 'Недостаточно данных для входа'),
+            (None, '', 400, 'Недостаточно данных для входа'),
+            ('', None, 400, 'Недостаточно данных для входа'),
+            ('wrong_login', 'wrong_password', 404, 'Учетная запись не найдена')
+        ]
+    )
+    def test_login_courier_invalid_data(self, login, password, expected_status, expected_message):
+        with allure.step('Создание payload'):
+            payload = {}
+            if login is not None:
+                payload['login'] = login
+            if password is not None:
+                payload['password'] = password
+
+        with allure.step(f'Авторизация курьера c payload {payload}'):
+            response = requests.post(LOGIN_COURIER_URL, json=payload)
+
+        with allure.step(f'Проверка, что пришел код {expected_status}'):
+            assert expected_status == response.status_code, f'Пришел код: {response.status_code}'
+
+        with allure.step('Проверка сообщения'):
+            actual_message = response.json().get("message")
+            assert actual_message == expected_message, f"Получили message: {actual_message}"
